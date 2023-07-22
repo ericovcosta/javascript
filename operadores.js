431f0bb5669c46a5d264c476080070057f416928
ericovcosta <professorerico@pi.senac.br>
2023-07-21 21:15:09 -0300

    aula1

---
+++ operadores.js
@@ -0,0 +1,30 @@
+//Aritméticos
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+let numeroUm = 5
+let numeroDois = 6
+let numeroTres = -1
+console.log(numeroDois + numeroTres)
+
+//Relacionais
+// > maior que
+// < menor que
+// != diferente
+//     !==
+// == igual
+//     ===
+// >= maior ou igual
+// <= menor ou igual
+
+let numeroString = '6'
+console.log(numeroDois === numeroString)
+// = Atribuição
+
+//Lógicos
+// && E
+// || OU
+// ! NÃO
+
+console.log(!(numeroDois === numeroString))
---
+++ variaveis.js
@@ -0,0 +1,16 @@
+//usando var ou nada
+// identificador
+//     atribuicao
+// valor
+//possível erro com undefined
+var nome="Érico"
+sobrenome="Costa"
+console.log(nome)
+
+//não permite a execução antes da criação (ERRO)
+let nome2 = 'Senac'
+console.log(typeof nome2)
+
+const pi=3.141923
+console.log(typeof pi)
+
